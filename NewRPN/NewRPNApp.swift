663231a5eadacdc7e10a6a6607a32c108fe2e6d7
Jib Ray <jib2ray@gmail.com>
2022-11-28 14:32:42 -0800

    Initial Commit

---
+++ NewRPN/NewRPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewRPNApp.swift
+//  NewRPN
+//
+//  Created by Jib Ray on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct NewRPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
